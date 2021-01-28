3f77e5f482e89a3e03b1b04e7a0f941dae5d8262
Nirvana <gui1050214471@163.com>
2021-01-28 23:47:19 +0800

    手写wordcount 的代码程序

--- src/main/java/org/hadoop/MyMapper.java
+++ src/main/java/org/hadoop/MyMapper.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 /**
  * @author Nirvana
@@ -12,13 +13,21 @@ import java.io.IOException;
  */
 public class MyMapper extends Mapper<Object, Text, Text, IntWritable> {
 
+    IntWritable count = new IntWritable(1);
+    Text str = new Text();
+
     @Override
     protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
         // super.map(key, value, context);
 
-        String[] split = value.toString().split(" ");
-        for (String str : split) {
-            context.write(new Text(str), new IntWritable(1));
+//        String[] split = value.toString().split(" ");
+//        for (String str : split) {
+//            context.write(new Text(str), count);
+//        }
+        StringTokenizer tokenizer = new StringTokenizer(value.toString());
+        while (tokenizer.hasMoreTokens()) {
+            str.set(tokenizer.nextToken());
+            context.write(str, count);
         }
     }
 }
--- src/main/java/org/hadoop/WordCount.java
+++ src/main/java/org/hadoop/WordCount.java
@@ -2,24 +2,35 @@ package org.hadoop;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.hadoop.mapred.TextOutputFormat;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
 import java.io.IOException;
 
 /**
  * @author Nirvana
  * @date 2021/1/26 23:07
+ *
+ * 编写完成之后将项目打包, 上传到服务器通过hadoop jar 的方式进行调用
  */
 public class WordCount {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        System.setProperty("HADOOP_USER_NAME", "root");
+
         Configuration config = new Configuration();
 
+        // 参数个性化. 这个里面会处理掉 -D 的配置参数
+        GenericOptionsParser parser = new GenericOptionsParser(config, args);
+        String[] others = parser.getRemainingArgs();
 
+        //config.set("mapreduce.app-submission.cross-platform", "true");
+        config.set("mapreduce.framework.name", "local");
         Job job = Job.getInstance(config);
-
+        //job.setJar("/Users/mac/Documents/GitHub/bigdata/target/bigdata-1.0-SNAPSHOT.jar");
         job.setJarByClass(WordCount.class);
 //      job.setJarByClass(MyJob.class);
 //      // Specify various job-specific parameters
@@ -28,20 +39,22 @@ public class WordCount {
 //      job.setInputPath(new Path("in"));
 //      job.setOutputPath(new Path("out"));
 
-        JobConf jobConf = new JobConf(config);
-        TextInputFormat.addInputPath(jobConf, new Path("/usr/data/input"));
+        TextInputFormat.addInputPath(job, new Path("/usr/data/input"));
 
         Path outputPath = new Path("/usr/data/output");
         // 保证输出文件的路径是唯一存在的，当路径存在进行删除操作
         if(outputPath.getFileSystem(config).exists(outputPath)) {
             outputPath.getFileSystem(config).delete(outputPath, true);
         }
-        TextOutputFormat.setOutputPath(jobConf, outputPath);
+
+        TextOutputFormat.setOutputPath(job, outputPath);
 
 //      job.setMapperClass(MyJob.MyMapper.class);
 //      job.setReducerClass(MyJob.MyReducer.class);
 
         job.setMapperClass(MyMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
         job.setReducerClass(MyReducer.class);
 //      // Submit the job, then poll for progress until the job is complete
 //      job.waitForCompletion(true);
